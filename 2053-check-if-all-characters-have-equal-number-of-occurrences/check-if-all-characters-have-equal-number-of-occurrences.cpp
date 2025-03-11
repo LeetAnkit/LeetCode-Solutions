872a29a1016cfe9f59319e059440f2b0e2e38534
Ankit Kumar <134221882+LeetAnkit@users.noreply.github.com>
2025-03-11 23:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (66.74%) - LeetSync

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    bool areOccurrencesEqual(string s) {
+
+       unordered_map<char , int> mp;
+
+     for ( char &c : s){
+        mp[c]++;
+     }
+
+        int  ct = mp[s[0]];
+
+        for( int i =0 ; i< s.length() ; i++){
+            if(mp[s[i]] != ct) return false;
+        }
+        return true;  
+    }
+};
