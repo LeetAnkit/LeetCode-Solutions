130b451232638f2e67afd2f1c555860abb58b3cc
Ankit Kumar <134221882+LeetAnkit@users.noreply.github.com>
2025-05-13 21:24:11 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (10.63%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string makeGood(string s) {
+         stack<char> st;
+
+
+        for( auto ch : s){
+            if(!st.empty() && abs(st.top() - ch ) == 32) {
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+
+
+        string result="";
+
+        while( !st.empty()){
+            result= st.top() + result;
+            st.pop();
+        }
+        return result;
+    }
+};
