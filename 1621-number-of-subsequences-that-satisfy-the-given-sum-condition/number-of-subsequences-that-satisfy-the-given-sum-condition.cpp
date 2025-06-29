240ffd4e19af5a5333696830385adeb64398ebd6
Ankit Kumar <134221882+LeetAnkit@users.noreply.github.com>
2025-06-29 20:03:13 +0530

    Time: 32 ms (52.57%) | Memory: 54.4 MB (34.63%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    int numSubseq(std::vector<int>& nums, int target) {
-        std::sort(nums.begin(), nums.end());
+    int numSubseq(vector<int>& nums, int target) {
+        
+        sort(nums.begin(), nums.end());
 
-        int left = 0;
-        int right = nums.size() - 1;
-        int mod = 1e9 + 7;
-        int count = 0;
+        int left =0;
+        int right =nums.size()-1;
+        int mod = 1e9+7;
+        int count =0;
 
-        // ✅ Allocate enough size: nums.size(), not nums.size() - 1
-        std::vector<int> pow(nums.size(), 1);
-        for (int i = 1; i < nums.size(); i++) {
+        vector<int>pow(nums.size(), 1);
+        for(int i =1; i< nums.size(); i++){
             pow[i] = (pow[i - 1] * 2) % mod;
         }
 
-        while (left <= right) {
-            if (nums[left] + nums[right] <= target) {
-                count = (count + pow[right - left]) % mod;
-                left++;
-            } else {
+        while(left <= right){
+            if((nums[left] + nums[right]) <= target){
+               count = (count+ pow[right- left]) % mod;
+
+               left++;
+
+            }
+            else{
                 right--;
             }
-        }
 
+        }
         return count;
     }
 };
