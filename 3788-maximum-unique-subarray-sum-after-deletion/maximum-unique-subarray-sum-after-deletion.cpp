ca56531ad0539dd6cf7509cb505f4cdfed2e1890
Ankit Kumar <134221882+LeetAnkit@users.noreply.github.com>
2025-07-25 19:40:03 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (86.99%) - LeetSync

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+
+        set<int> s;
+        int ans =0;
+        int maxEle = INT_MIN;
+
+        for( int i =0; i< nums.size(); i++){
+            maxEle = max(nums[i], maxEle);
+
+            if(nums[i] > 0 && !s.count(nums[i])){
+                ans+=nums[i];
+                s.insert(nums[i]);
+            }
+        }
+        return ans ==0? maxEle: ans;
+    }
+};
