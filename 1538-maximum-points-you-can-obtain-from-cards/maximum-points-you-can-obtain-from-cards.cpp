1475c32ae76c1aa6160f2ee475bfb3dae6e3a20d
Ankit Kumar <134221882+LeetAnkit@users.noreply.github.com>
2025-07-29 20:55:41 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (47.01%) - LeetSync

---
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        int total = 0;
+        
+        for (int point : cardPoints) {
+            total += point;
+        }
+
+        // Window size of cards we're NOT picking
+        int windowSize = n - k;
+        int minSum = 0;
+        
+        for (int i = 0; i < windowSize; ++i) {
+            minSum += cardPoints[i];
+        }
+        
+        int currSum = minSum;
+        for (int i = windowSize; i < n; ++i) {
+            currSum += cardPoints[i] - cardPoints[i - windowSize];
+            minSum = std::min(minSum, currSum);
+        }
+        
+        return total - minSum;
+    }
+};
