2b060a3bb071806b1df3bad7f391442e3249e159
Ankit Kumar <134221882+LeetAnkit@users.noreply.github.com>
2025-08-14 16:03:35 +0530

    Time: 8 ms (27.30%) | Memory: 27.1 MB (81.00%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findPosition(vector<int>& inorder, int ele, int n) {
+        for (int i = 0; i < n; i++) {
+            if (inorder[i] == ele) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int &index, int inorderStart, int inorderEnd, int n) {
+        if (index >= n || inorderStart > inorderEnd) {
+            return nullptr;
+        }
+
+        int ele = preorder[index++];
+        TreeNode* root = new TreeNode(ele);
+
+        int position = findPosition(inorder, ele, n);
+
+        root->left = solve(preorder, inorder, index, inorderStart, position - 1, n);
+        root->right = solve(preorder, inorder, index, position + 1, inorderEnd, n);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = inorder.size();
+        int preorderIndex = 0;
+        TreeNode* ans = solve(preorder, inorder, preorderIndex, 0, n - 1, n);
+        return ans;
+    }
+};
