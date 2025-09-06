82a4b60a36191584feb52d986e0a01478677d5ca
Ankit Kumar <134221882+LeetAnkit@users.noreply.github.com>
2025-09-06 15:00:50 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (78.25%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-       //  first replacing all the same coming elements by INT_MAX// you can also represent as INT_MIN
-        for ( int i =1 ; i< nums.size(); i++){
-            if (nums[i]== nums[i-1]){
-                nums[i-1] = INT_MAX;
+        if(nums.empty()) return 0;
+
+        int j =0 ;
+        for( int i =1 ; i< nums.size() ; i++){
+            if( nums[i] != nums[j]){
+                j++;
+                nums[j] = nums[i];
             }
-        }
-        // for sorting purpose 
-        sort (nums.begin() , nums.end());
 
+        }
+        return j+1;
         
-        int count=0;
-        for( int i=0; i<nums.size () ; i++){
-            if ( nums[i]!=  INT_MAX) count++;
-
-        }
-        return count;
-
     }
 };
